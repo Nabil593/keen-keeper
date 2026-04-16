57b34aa89faf7365fc5994a1cb0cf8b34185f66c
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-16 20:10:07 +0600

    filter logic added

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -5,29 +5,34 @@ import Image from "next/image";
 import call from "@/app/assets/call.png";
 import text from "@/app/assets/text.png";
 import video from "@/app/assets/video.png";
+import { useState } from "react";
 
 const TimelinePage = () => {
 
     const { interactions } = useInteractions();
+    const [filterType, setFilterType] = useState("All");
+
+    const filteredInteractions = filterType === "All" ? interactions : interactions.filter(filtered => filtered.action === filterType);
+
 
     return (
         <div className="max-w-[1300px] mx-auto w-full my-20 space-y-8">
             <div className="space-y-8">
                 <h2 className="text-5xl font-bold">Timeline</h2>
-                <select defaultValue="Fillter Timeline" className="select appearance-none bg-white">
-                    <option disabled={true} >Fillter Timeline</option>
-                    <option>Call</option>
-                    <option>Text</option>
-                    <option>Video</option>
+                <select value={filterType} onChange={(e) => setFilterType(e.target.value)} className="select appearance-none bg-white border border-gray-300 rounded p-2">
+                    <option value="All">All Timeline</option>
+                    <option value="call">Call</option>
+                    <option value="text">Text</option>
+                    <option value="video">Video</option>
                 </select>
             </div>
 
             <div>
                 <div className="flex flex-col gap-6">
                     {
-                        interactions.map(interact => (
+                        filteredInteractions.map(interact => (
                             <div key={interact.id} className="bg-white shadow-lg p-4 rounded-sm flex gap-8">
-                                <Image src={interact.action === 'call' ? call : interact.action === 'text' ? text : video} alt={interact.action} width={40} height={40}/>
+                                <Image src={interact.action === 'call' ? call : interact.action === 'text' ? text : video} alt={interact.action} />
                                 <div>
                                     <p>{interact.friendName}</p>
                                     <p>{interact.timeDate}</p>
