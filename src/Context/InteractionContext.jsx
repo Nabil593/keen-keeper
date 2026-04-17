867020928c128d225549c75fb23bba38f42fe12c
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-17 17:55:53 +0600

    Timeline compeleted

--- src/Context/InteractionContext.jsx
+++ src/Context/InteractionContext.jsx
@@ -2,33 +2,39 @@
 
 import React, { createContext, useContext, useState } from 'react';
 
-// ১. মেইন কনটেক্সট তৈরি (এটি বাইরে থাকবে)
 export const InteractionContext = createContext();
 
-const InteractionProvider = ({ children }) => { // children প্রপ হিসেবে আসবে
+const InteractionProvider = ({ children }) => {
 
-    const [interactions, setInteractions] = useState([]); // অনেকগুলো ডাটা রাখার জন্য Array [] ব্যবহার করা ভালো
+    const [interactions, setInteractions] = useState([]);
 
     const addInteraction = (friend, type) => {
         const newEntry = {
             id: Date.now(),
             friendName: friend.name,
             action: type,
-            timeDate: new Date().toLocaleString()
+            timeDate: new Date().toLocaleDateString('en-US', {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric'
+            }),
+            todayTime: new Date().toLocaleTimeString('en-US', {
+                hour: 'numeric',
+                minute: '2-digit',
+                hour12: true
+            })
         };
-        // নতুন ডাটাকে আগের ডাটাগুলোর সামনে যোগ করা
+
         setInteractions(prev => [newEntry, ...prev]);
     };
 
     return (
-        // ২. Provider এর মাধ্যমে ভ্যালু পাস করা
         <InteractionContext.Provider value={{ interactions, addInteraction }}>
-            {children} 
+            {children}
         </InteractionContext.Provider>
     );
 };
 
-// ৩. ডাটা ব্যবহারের জন্য একটি কাস্টম হুক (সহজ করার জন্য)
 export const useInteractions = () => useContext(InteractionContext);
 
 export default InteractionProvider;
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -11,15 +11,19 @@ const TimelinePage = () => {
 
     const { interactions } = useInteractions();
     const [filterType, setFilterType] = useState("All");
+    const [search, setSearch] = useState("");
 
     const filteredInteractions = filterType === "All" ? interactions : interactions.filter(filtered => filtered.action === filterType);
 
+    const searchedInteractions = search
+        ? filteredInteractions.filter((interact) => interact.friendName.toLowerCase().includes(search.toLowerCase()))
+        : filteredInteractions;
 
     return (
-        <div className="max-w-[1300px] mx-auto w-full my-20 space-y-8">
+        <div className="max-w-[1200px] mx-auto w-full my-20 space-y-8 mb-30 lg:px-12 md:px-6 px-4">
 
-            <h2 className="text-5xl font-bold">Timeline</h2>
-            <div className="flex lg:flex-row md:flex-row flex-col gap-8 justify-between">
+            <h2 className="lg:text-5xl md:text-4xl text-4xl font-bold">Timeline</h2>
+            <div className="flex lg:flex-row md:flex-row flex-col-reverse gap-8 justify-between">
                 <div className="w-full">
                     <select value={filterType} onChange={(e) => setFilterType(e.target.value)} className="select appearance-none bg-white border border-gray-300 rounded p-2">
                         <option value="All">All Timeline</option>
@@ -42,19 +46,19 @@ const TimelinePage = () => {
                             <path d="m21 21-4.3-4.3"></path>
                         </g>
                     </svg>
-                    <input type="search" required placeholder="Search" />
+                    <input onChange={(e) => setSearch(e.target.value)} type="search" required placeholder="Search" />
                 </label>
             </div>
 
             <div>
                 <div className="flex flex-col gap-6">
                     {
-                        filteredInteractions.map(interact => (
-                            <div key={interact.id} className="bg-white shadow-lg p-4 rounded-sm flex gap-8">
-                                <Image src={interact.action === 'call' ? call : interact.action === 'text' ? text : video} alt={interact.action} />
+                        searchedInteractions.map(interact => (
+                            <div key={interact.id} className="bg-white shadow-sm p-4 rounded-sm flex gap-8">
+                                <Image src={interact.action === 'call' ? call : interact.action === 'text' ? text : video} alt={interact.action} width={40} height={20}/>
                                 <div>
-                                    <p>{interact.friendName}</p>
-                                    <p>{interact.timeDate}</p>
+                                    <p className="text-[20px] font-medium text-[#244D3F]">{interact.action.charAt(0).toUpperCase() + interact.action.slice(1)}<span className="text-gray-500 text-[16px]"> with {interact.friendName}</span></p>
+                                    <p className="text-gray-500">{interact.timeDate} {interact.todayTime}</p>
                                 </div>
                             </div>
                         ))
